302ec33debb3adce4a4e9f88e7a1ccf4ff07e604
laminelawal <songnel9@gmail.com>
2024-06-26 23:35:59 +0200

    New features

---
+++ 0.2_Pyramid_Generator/dist/index.c36f364e.js
@@ -0,0 +1,25 @@
+const result = document.querySelector(".result");
+const body = document.querySelector("body");
+body.style.backgroundColor = "black";
+console.log(body);
+let character = "#";
+let count = 8;
+let rows = [];
+// function padRow(rowNumber, rowCount) {}
+for(let i = 1; i <= count; i++)//To invert the tree you can use unshift by replacing push.
+rows.push(".".repeat(count - i) + character.repeat(2 * i - 1) + ".".repeat(count - i));
+//Before u print the value in your HTML div check it in the console while you
+// are working on it.
+// for (const row of rows) {
+//   result = restest + "\n" + row;
+// }
+rows.forEach((e)=>{
+    let p = document.createElement("p");
+    p.style.color = "darkblue";
+    p.style.padding = "0 20px";
+    p.append(e);
+    result.appendChild(p);
+});
+console.log(character, count, "ROWS", rows, "RESTEST", result);
+
+//# sourceMappingURL=index.c36f364e.js.map
